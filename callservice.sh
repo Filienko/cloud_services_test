cf35054c8b4f63b13b0665c6cd0f2f8754a7d0f6
tkr25 <105318499+tkr25@users.noreply.github.com>
2023-12-05 15:10:53 -0800

    Update callservice.sh
    removed row, col added raw bill text

--- callservice.sh
+++ callservice.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
 # CSV data
-csv_data='order_id,product_id,seller_id,date,num_pieces_sold
-1,0,0,2022-07-10,62
-2,1,1,2022-07-11,45
-3,2,0,2022-07-12,30
-4,1,2,2022-07-13,75
-5,2,0,2022-07-14,20'
+csv_data='order_id,product_id,seller_id,date,num_pieces_sold,bill_raw_text
+1,0,0,2022-07-10,62,sfgsdfgsdfgs
+2,1,1,2022-07-11,45,sdfgsdfgadfg
+3,2,0,2022-07-12,30,sdfgadfgsdf
+4,1,2,2022-07-13,75,sdfgafdg
+5,2,0,2022-07-14,20,sdfgsdfgfd'
 
 # Escape newlines in CSV data
 escaped_csv_data=$(echo "$csv_data" | awk '{printf "%s\\n", $0}')
 
 # JSON object to pass to Lambda Function
 json='{
-  "row": 5,
-  "col": 5,
   "bucketname": "projectbucketdatarecieved",
   "filename": "test.csv",
   "CsvData": "'"$escaped_csv_data"'"
